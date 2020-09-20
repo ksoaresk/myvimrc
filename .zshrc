488f99fcadee2f44e3fdc461c0ddd160dd08137e
karloswebmaster <karloswebmaster@gmail.com>
2020-09-20 01:06:00 -0300

    #time 1h 30m Modifica script de instalação de depenedencias.

--- .zshrc
+++ .zshrc
@@ -72,7 +72,7 @@ DISABLE_AUTO_TITLE="true"
 # Example format: plugins=(rails git textmate ruby lighthouse)
 # Add wisely, as too many plugins slow down shell startup.
 plugins=(git)
-
+# zsh-autosuggestions zsh-syntax-highlighting
 source $ZSH/oh-my-zsh.sh
 
 # User configuration
--- install.sh
+++ install.sh
@@ -43,27 +43,48 @@ fi
 if [ ! -d ~/.config/nvim ];
 then
 	sudo apt-get install -y neovim
+	mkdir -p ~/.config/nvim
 fi
 
 rm -rf ~/.config/nvim/init.vim
 ln -S ~/.config/nvim/init.vim ~/.myvimrc/.vimrc
 
-echo -e "Configuração NVIM realizada com sucesso.\n
+echo -e "Configuração NVIM realizada com sucesso.
 	Agora abra o seu NVIM e digite no modo normal \033[01;32m:PlugInstall\033[01;37!"
 
 	
 rm -rf ~/.vimrc
 ln -S ~/.vimrc ~/.myvimrc/.vimrc
 
-echo -e "Configuração VIM realizada com sucesso.\n
+echo -e "Configuração VIM realizada com sucesso.
 	Agora abra o seu VIM e digite no modo normal \033[01;32m:PlugInstall\033[01;37!"
 
 rm -rf ~/.ideavim
 ln -S ~/.ideavim ~/.myvimrc/.ideavim
 
-echo -e "Configuração IDEAVIM realizada com sucesso." \n
+echo -e "Configuração IDEAVIM realizada com sucesso." 
+
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+echo  "Configuração FZF realizada com sucesso." 
+
+#sudo apt install zsh -y
+sudo apt install zsh wget git fonts-powerline -y
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 rm -rf ~/.zshrc
 ln -S ~/.zshrc ~/.myvimrc/.zshrc
 
-echo -e "Configuração ZSH realizada com sucesso." \n
+echo -e "Configuração ZSH realizada com sucesso." 
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+echo "ZSH: Plugin AutoComplete"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "ZSH: Plugin Hilight"
+
+rm -rf ~/.tmux.conf
+ln -S ~/.tmux.conf ~/.myvimrc/.tmux.conf
+
+echo -e "Configuração ZSH realizada com sucesso." 
